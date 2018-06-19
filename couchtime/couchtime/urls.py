4d7008c1c7d0ff719399131e1773a9904b8056dc
xavrb <javier.mejia@protonmail.ch>
2018-06-18 21:46:25 -0500

    added movie resource to urls.py

--- couchtime/couchtime/urls.py
+++ couchtime/couchtime/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from api.resources import movieResource
+from django.conf.urls import url, include
+
+movie_resource = movieResource()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^api/', include(movie_resource.urls)),
 ]
