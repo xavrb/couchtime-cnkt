0d747413860a43fcb611d0afe079a4ffca0a80ed
xavrb <javier.mejia@protonmail.ch>
2018-06-18 22:40:36 -0500

    added auth to post ep

--- couchtime/api/resources.py
+++ couchtime/api/resources.py
@@ -1,7 +1,10 @@
 from tastypie.resources import ModelResource
 from api.models import Movie
+from tastypie.authorization import Authorization
+
 
 class MovieResource(ModelResource):
     class Meta:
         queryset = Movie.objects.all()
         resource_name = 'movie'
+        authorization = Authorization()
